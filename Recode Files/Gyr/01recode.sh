8f68fda1746ff6e0de6fbf7a2d127f1d8228f5a6
elladodd6 <78565434+elladodd6@users.noreply.github.com>
2021-04-26 16:24:52 -0400

    Update 01recode.sh

--- Recode Files/Gyr/01recode.sh
+++ Recode Files/Gyr/01recode.sh
@@ -13,4 +13,5 @@
 module load plink/1.90.beta.4.4
 #module load plink/2.00a2.3LM
 
-plink --file cattle__732993variants__27individuals --cow --noweb --recode12
+cp cattle__732993variants__27individuals.ped gyr.ped
+plink --file gyr --cow --noweb --recode12
