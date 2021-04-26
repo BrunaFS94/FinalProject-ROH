a31fa35bb0a07f57ed717fafb973b77bb4e8a82b
elladodd6 <78565434+elladodd6@users.noreply.github.com>
2021-04-26 16:27:04 -0400

    Create map_holstein.R

---
+++ Holstein/map_holstein.R
@@ -0,0 +1,8 @@
+map_hol<-read.table('cattle__732993variants__60individuals.map', header = F)
+map_hol<-data.frame(seq(1:732993), map_hol)
+names(map_hol)<-c('N','chr','SNP','Distance','Position')
+map_hol<-map_hol[,c(2,1,5)]
+map_hol$zero<-0
+map_hol<-map_hol[,c(1,2,4,3)]
+names(map_hol)<-c('Chromosome','Name','Distance','Position')
+write.table(map_hol,"holstein.map", quote = F, sep= " ", row.names = FALSE, col.names = FALSE)
