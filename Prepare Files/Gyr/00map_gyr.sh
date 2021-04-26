f8752afeca3c53060a27d67c51745565ce5e888e
elladodd6 <78565434+elladodd6@users.noreply.github.com>
2021-04-26 15:45:55 -0400

    Create 00map_gyr.sh

---
+++ Prepare Files/Gyr/00map_gyr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=make_binary
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --mem=30G
+#SBATCH --partition=mcbstudent
+#SBATCH --qos=mcbstudent
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=first.last@uconn.edu
+
+
+module load R/3.6.3
+
+Rscript map_gyr.R
