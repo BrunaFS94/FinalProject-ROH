9b38bd86a44545c5836456fe44c89e4a1a5a4fdf
elladodd6 <78565434+elladodd6@users.noreply.github.com>
2021-04-27 14:37:25 -0400

    Create Descriptive_stat_hfd.R

---
+++ Hereford/Descriptive_stat_hfd.R
@@ -0,0 +1,23 @@
+# hereford
+
+ 
+
+taurus.genome<- 2489.37
+
+ 
+
+mean(hom.indiv$KBAVG) # 4172.922
+
+ 
+
+mean(hom.indiv$KB) # 
+
+ 
+
+hom.indiv$f<- (hom.indiv$KB / 2489.37)/1000
+
+ 
+
+mean(hom.indiv$f) # 20.62%
+
+ 
