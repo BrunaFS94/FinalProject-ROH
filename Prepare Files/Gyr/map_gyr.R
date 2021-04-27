18305470263d91142db712b6aac063b2cb349354
elladodd6 <78565434+elladodd6@users.noreply.github.com>
2021-04-27 14:46:29 -0400

    Delete Prepare Files directory

--- Prepare Files/Gyr/map_gyr.R
+++
@@ -1,8 +0,0 @@
-map_gyr<-read.table('cattle__732993variants__27individuals.map', header = F)
-map_gyr<-data.frame(seq(1:732993), map_gyr)
-names(map_gyr)<-c('N','chr','SNP','Distance','Position')
-map_gyr<-map_gyr[,c(2,1,5)]
-map_gyr$zero<-0
-map_gyr<-map_gyr[,c(1,2,4,3)]
-names(map_gyr)<-c('Chromosome','Name','Distance','Position')
-write.table(map_gyr,"gyr.map", quote = F, sep= " ", row.names = FALSE, col.names = FALSE)
--- Prepare Files/Hereford/map_hereford.R
+++
@@ -1,8 +0,0 @@
-map_hfd<-read.table('cattle__732993variants__35individuals.map', header = F)
-map_hfd<-data.frame(seq(1:732993), map_hfd)
-names(map_hfd)<-c('N','chr','SNP','Distance','Position')
-map_hfd<-map_hfd[,c(2,1,5)]
-map_hfd$zero<-0
-map_hfd<-map_hfd[,c(1,2,4,3)]
-names(map_hfd)<-c('Chromosome','Name','Distance','Position')
-write.table(map_hfd,"hereford.map", quote = F, sep= " ", row.names = FALSE, col.names = FALSE)
