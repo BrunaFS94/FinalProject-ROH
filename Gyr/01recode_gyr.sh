dbef590b66dd8600b2f38c950dd49d4b718fd12e
elladodd6 <78565434+elladodd6@users.noreply.github.com>
2021-04-27 14:38:49 -0400

    Create 01recode_gyr.sh

---
+++ Gyr/01recode_gyr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=make_binary
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --mem=30G
+#SBATCH --partition=mcbstudent
+#SBATCH --qos=mcbstudent
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=first.last@uconn.edu
+
+#plink/1.07
+module load plink/1.90.beta.4.4
+#module load plink/2.00a2.3LM
+
+cp cattle__732993variants__27individuals.ped gyr.ped
+plink --file gyr --cow --noweb --recode12
