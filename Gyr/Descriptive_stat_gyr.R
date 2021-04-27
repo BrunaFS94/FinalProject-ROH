14448ef343e860d84ea7014cc91372a43d76d500
elladodd6 <78565434+elladodd6@users.noreply.github.com>
2021-04-27 14:45:45 -0400

    Create Descriptive_stat_gyr.R

---
+++ Gyr/Descriptive_stat_gyr.R
@@ -0,0 +1,21 @@
+#gyr
+
+ 
+
+taurus.genome<- 2489.37
+
+ 
+
+mean(hom.indiv$KBAVG) # 3267.363
+
+ 
+
+mean(hom.indiv$KB) # 170585.5
+
+ 
+
+hom.indiv$f<- (hom.indiv$KB / 2489.37)/1000
+
+ 
+
+mean(hom.indiv$f) # 6.85%
