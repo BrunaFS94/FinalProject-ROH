86162ed19e1a7990958c2029f6eafc9c808156a7
BrunaFS94 <78558238+BrunaFS94@users.noreply.github.com>
2021-05-04 11:38:34 -0400

    Update Descriptive_stat_gyr.R

--- Gyr/Descriptive_stat_gyr.R
+++ Gyr/Descriptive_stat_gyr.R
@@ -1,21 +1,27 @@
-#gyr
+#######################################################
+#computing statistics and inbreeding coefficients(f)
+######################################################
 
- 
+#First, set the accordingly directory 
+hom<-  read.table("holstein-ROH.hom", h=T)
+hom.indiv<-  read.table("holstein-ROH.hom.indiv", h=T)
 
+#number of segments
+n_segments<- nrow(hom)
+
+#computing ROH mean_length for each individual and general from the samples
+mean.individual.ROH<- round(mean(hom.indiv$KBAVG),2)
+mean.general.ROH<- round(mean(hom.indiv$KB),2)
+
+#inbreeding coefficient
 taurus.genome<- 2489.37
-
- 
-
-mean(hom.indiv$KBAVG) # 3267.363
-
- 
-
-mean(hom.indiv$KB) # 170585.5
-
- 
-
 hom.indiv$f<- (hom.indiv$KB / 2489.37)/1000
+mean_f<- round(mean(hom.indiv$f),3) 
 
- 
 
-mean(hom.indiv$f) # 6.85%
+# put them in a table:
+tble <- data.frame( Summaries = c(n_segments, mean.individual.ROH, mean.general.ROH,
+                                  mean_f))
+
+row.names(tble) <- c("ROH sample number", "Individual ROH length", "Total ROH length",
+                     "Inbreeding coefficient")
